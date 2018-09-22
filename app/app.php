e3c483a4138141c9f277d3f69d8c100894b40132
Erica Wright <erica.wright@soliddigital.com>
2018-09-22 14:32:03 -0700

    Set up php file upload and send to class

--- app/app.php
+++ app/app.php
@@ -16,18 +16,41 @@
     Request::enableHttpMethodParameterOverride();
 
     $app->get("/", function() use ($app) {
-        $blank_form = array();
-        return $app['twig']->render('home.html.twig', array('blank_form' => $blank_form));
+        $error_message = '';
+        return $app['twig']->render('home.html.twig', array('error_message' => $error_message));
     });
 
     $app->post("/upload-csv", function() use ($app) {
-        $file = $_POST['file'];
-        $blank_form = array();
-        if (!$file || strtolower(pathinfo($file, PATHINFO_EXTENSION)) != 'csv') {
-            array_push($blank_form, "empty");
-            return $app['twig']->render('home.html.twig', array('blank_form' => $blank_form));
+        $filename = $_FILES['file']['name'];
+        $target_dir = "uploads/";
+        $target_file = $target_dir . basename($filename);
+        $fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        $uploadFile = move_uploaded_file(!$_FILES['file']['tmp_name'], $target_file);
+        if($fileType != "csv") {
+            $error_message = "Sorry, only CSV files are allowed.";
+            return $app['twig']->render('home.html.twig', array('error_message' => $error_message));
+        } else if (!$filename) {
+            $error_message = 'Please submit a file for processing.';
+            return $app['twig']->render('home.html.twig', array('error_message' => $error_message));
+        } else if ($_FILES['file']['error'] > 0) {
+            $error_message = 'An error ocurred when uploading that file. Please try again.';
+            return $app['twig']->render('home.html.twig', array('error_message' => $error_message));
         } else {
-            return $app['twig']->render('output.html.twig');
+            if (move_uploaded_file($_FILES['file']['tmp_name'], $target_file)) {
+                //Open saved csv file and translate to array to output data
+                $handle = fopen($target_file, 'r');
+                if ($handle) {
+                    $new_products_table = new ProfitCalculator($handle);
+                    $csv_table_array = $new_products_table->getCSVArray();
+                } else {
+                    $csv_table_array = 'Unable to open file, please try again.';
+                }
+                return $app['twig']->render('output.html.twig', array('product_table' => $csv_table_array));
+            } else {
+                $error_message = 'Error uploading file, check destination is writeable.';
+                return $app['twig']->render('home.html.twig', array('error_message' => $error_message));
+            }
+
         }
     });
     
--- src/ProfitCalculator.php
+++ src/ProfitCalculator.php
@@ -1,4 +1,12 @@
 <?php
     class ProfitCalculator {
-        
+        private $csv_table_array;
+
+        function __construct($file) {
+            $this->csv_table_array = fgetcsv($file);
+        }
+
+        function getCSVArray() {
+            return $this->csv_table_array;
+        }
     }
