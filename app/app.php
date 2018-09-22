f0925e68819af5e777de36ba634380c52a3bffd8
Erica Wright <erica.wright@soliddigital.com>
2018-09-22 11:05:33 -0700

    Set up basic styling and initial home page route

---
+++ app/app.php
@@ -0,0 +1,18 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__."/../vendor/autoload.php";
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+    $app['debug'] = true;
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('home.html.twig');
+    });
+
+    
+    
+    return $app;
